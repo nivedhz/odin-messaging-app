9584b3afe6068a20c891d851e30e89aaec1a330b
Nivedh E R <nivedher12@gmail.com>
2026-09-08 20:27:12 +0530

    feat(/): added a hero section and havbar for /

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <h1>hello, world</h1>;
+  return (
+    <div className="relative min-h-screen max-w-screen">
+      <Hero />
+    </div>
+  );
 }
