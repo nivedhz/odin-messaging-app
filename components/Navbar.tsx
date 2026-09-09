3704d85274218f1b816bf90bb4b42897a06193f9
Nivedh E R <nivedher12@gmail.com>
2026-09-09 17:48:45 +0530

    chore(remove nav): removed the unnecessary nav links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,18 +2,6 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import { Send } from "lucide-react";
 
-interface Link {
-  href: string;
-  label: string;
-}
-
-const links: Link[] = [
-  { href: "#about", label: "About" },
-  { href: "#features", label: "Features" },
-  { href: "#contact", label: "Contact" },
-  { href: "#faq", label: "FAQ" },
-];
-
 const Navbar = () => {
   return (
     <div className="px-30 py-3 flex items-center justify-between">
@@ -21,13 +9,6 @@ const Navbar = () => {
         <Send width={30} color="#c7baed" />
         Sendzy
       </h1>
-      <nav className="flex gap-8">
-        {links.map((link) => (
-          <Link href={link.href} key={link.href} className="text-sm">
-            {link.label}
-          </Link>
-        ))}
-      </nav>
       <div className="flex gap-2">
         <Link href={"login"}>
           <Button variant={"outline"} className={"bg-transparent"}>
