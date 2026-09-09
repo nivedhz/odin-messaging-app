02332a7f3294a657dcae0976d11958260ec404e3
Nivedh E R <nivedher12@gmail.com>
2026-09-09 07:27:25 +0530

    refactor(FloatingCard): extracted to it's own component file

---
+++ components/FloatingCard.tsx
@@ -0,0 +1,26 @@
+import SpotlightCard from "@/components/SpotlightCard";
+import {
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Bell } from "lucide-react";
+
+const FloatingCard = ({ name, message }: { name: string; message: string }) => {
+  return (
+    <SpotlightCard className="w-72 shadow-2xl bg-transparent animate-[float_2s_ease-in-out_infinite] flex flex-col gap-2 border-muted-foreground/20 justify-between">
+      <CardHeader className="flex items-center justify-between">
+        <CardTitle className="text-base">{name}</CardTitle>
+        <CardAction>
+          <Bell width={20} />
+        </CardAction>
+      </CardHeader>
+      <CardContent className="">
+        <p className="text-sm text-muted-foreground">{message}</p>
+      </CardContent>
+    </SpotlightCard>
+  );
+};
+
+export default FloatingCard;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,5 @@
-import SpotlightCard from "@/components/SpotlightCard";
 import { Button } from "@/components/ui/button";
-import {
-  CardAction,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Bell } from "lucide-react";
+import FloatingCard from "./FloatingCard";
 
 const Hero = () => {
   return (
@@ -36,32 +29,16 @@ const Hero = () => {
           </div>
         </div>
         <div className="flex items-center justify-between min-w-full px-60">
-          <SpotlightCard className="w-72 shadow-2xl bg-transparent tranform translate-y-3/4 animate-[float_2s_ease-in-out_infinite] flex flex-col gap-2 border-muted-foreground/20 justify-between">
-            <CardHeader className="flex items-center justify-between">
-              <CardTitle className="text-base">Humpty</CardTitle>
-              <CardAction>
-                <Bell width={20} />
-              </CardAction>
-            </CardHeader>
-            <CardContent className="">
-              <p className="text-sm text-muted-foreground">
-                I&apos;m sitting somewhere high.
-              </p>
-            </CardContent>
-          </SpotlightCard>
-          <SpotlightCard className="w-72 shadow-2xl bg-transparent animate-[float_2s_ease-in-out_infinite] flex flex-col gap-2 border-muted-foreground/20 delay-200">
-            <CardHeader className="flex items-center justify-between">
-              <CardTitle className="text-base">Cindrella</CardTitle>
-              <CardAction>
-                <Bell width={20} />
-              </CardAction>
-            </CardHeader>
-            <CardContent className="">
-              <p className="text-sm text-muted-foreground">
-                Ah shit! I just lost my shoe.
-              </p>
-            </CardContent>
-          </SpotlightCard>
+          <div className="transform translate-y-12.5 ">
+            <FloatingCard
+              name="Humpty"
+              message="I am sitting on a high place"
+            />
+          </div>
+          <FloatingCard
+            name="Cindrella"
+            message="Ah shit! I just lost my shoe."
+          />
         </div>
       </div>
     </>
