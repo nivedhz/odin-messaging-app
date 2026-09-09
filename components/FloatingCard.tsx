1430efb18d1a414eed66471cee807e7dc79b7663
Nivedh E R <nivedher12@gmail.com>
2026-09-09 17:54:53 +0530

    feat(FloatingCard): added optional className for inserting tailwind classes

--- components/FloatingCard.tsx
+++ components/FloatingCard.tsx
@@ -7,9 +7,19 @@ import {
 } from "@/components/ui/card";
 import { Bell } from "lucide-react";
 
-const FloatingCard = ({ name, message }: { name: string; message: string }) => {
+const FloatingCard = ({
+  name,
+  message,
+  className,
+}: {
+  name: string;
+  message: string;
+  className?: string;
+}) => {
   return (
-    <SpotlightCard className="w-72 shadow-2xl bg-transparent animate-[float_2s_ease-in-out_infinite] flex flex-col gap-2 border-muted-foreground/20 justify-between">
+    <SpotlightCard
+      className={`w-72 shadow-2xl bg-transparent animate-[float_2s_ease-in-out_infinite] flex flex-col gap-2 border-muted-foreground/20 justify-between ${className}`}
+    >
       <CardHeader className="flex items-center justify-between">
         <CardTitle className="text-base">{name}</CardTitle>
         <CardAction>
