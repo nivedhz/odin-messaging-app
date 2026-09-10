38a8474ca6aa5548cf116a7e5054b0b1936a3541
Nivedh E R <nivedher12@gmail.com>
2026-09-10 18:52:04 +0530

    refactor(convention): add header instead of div for navbar container

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Send } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="px-10 md:px-20 lg:px-40 py-3 flex items-center justify-between">
+    <header className="px-10 md:px-20 lg:px-40 py-3 flex items-center justify-between">
       <h1 className="text-lg font-medium flex items-center">
         <Send width={30} color="#c7baed" />
         Sendzy
@@ -24,7 +24,7 @@ const Navbar = () => {
           </Button>
         </Link>
       </div>
-    </div>
+    </header>
   );
 };
 
