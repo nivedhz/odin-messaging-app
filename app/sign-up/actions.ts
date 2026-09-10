da7ced089f2ad986da2056727cc92fb21fb52a7d
Nivedh E R <nivedher12@gmail.com>
2026-09-10 17:13:15 +0530

    feat(getFormInfo and handleSignUp): added a basic axios post for signup

--- app/sign-up/actions.ts
+++ app/sign-up/actions.ts
@@ -1,9 +1,41 @@
 "use server";
 
-export async function handleSignUp(formData: FormData) {
-  const username = formData.get("username");
-  const email = formData.get("email");
-  const password = formData.get("password");
+import axios from "axios";
 
-  console.log(username, email, password);
+interface FormInfo {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const getFormInfo = (formData: FormData): FormInfo => {
+  const username = formData.get("username")?.toString() ?? "";
+  const email = formData.get("email")?.toString() ?? "";
+  const password = formData.get("password")?.toString() ?? "";
+
+  if (!username.trim() || !email.trim() || !password.trim()) {
+    throw new Error("Missing required fields");
+  }
+  return {
+    username,
+    email,
+    password,
+  };
+};
+
+export async function handleSignUp(formData: FormData) {
+  const { username, email, password } = getFormInfo(formData);
+
+  try {
+    const response = await axios.post("/api/auth/signup", {
+      username,
+      email,
+      password,
+    });
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
