bb676e5cb378562e951c6f0eb0384584d4e51ccd
Nivedh E R <nivedher12@gmail.com>
2026-09-11 18:31:14 +0530

    feat(Sessions): add get, create and delete session functions with helpers

---
+++ lib/session.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import { JWTPayload, SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+
+const SECRET_KEY = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(SECRET_KEY);
+const expiresAt = new Date(Date.now() + 60 * 60 * 1000);
+
+export async function encrypt(payload: JWTPayload): Promise<string> {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime(expiresAt)
+    .sign(encodedKey);
+}
+
+export async function decrypt(token: string): Promise<JWTPayload | null> {
+  try {
+    const { payload } = await jwtVerify(token, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (_err) {
+    return null;
+  }
+}
+
+export async function createSession(userId: string): Promise<void> {
+  const session = await encrypt({ userId, expiresAt });
+
+  const cookieStore = await cookies();
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    path: "/",
+    expires: expiresAt,
+  });
+}
+
+export async function deleteSession(): Promise<void> {
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
+}
+
+export async function getSession(): Promise<JWTPayload | null> {
+  const cookieStore = await cookies();
+  const session = cookieStore.get("session");
+  if (!session) return null;
+  return await decrypt(session.value);
+}
