abe9291513c610aa4a13ed664c5c733a0b83daad
Nivedh E R <nivedher12@gmail.com>
2026-09-12 18:12:49 +0530

    chore(unused vars): made no warning when ununsed vars are prefixed by _

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,12 +15,14 @@ const eslintConfig = defineConfig([
   ]),
   {
     rules: {
-      "no-unused-vars": [
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
         "warn",
         {
           argsIgnorePattern: "^_",
           varsIgnorePattern: "^_",
           caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
         },
       ],
     },
